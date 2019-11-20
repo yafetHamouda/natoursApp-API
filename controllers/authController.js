789e705d9b20e27086ced8eff22812c8c9aa1c16
yafetHamouda <yafet.hamouda1@gmail.com>
2019-11-20 18:45:55 +0100

    added email handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -166,17 +166,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   //3) send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password ? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n if you didn't forget your passwor, please ignore this email.`;
   try {
-    /*     await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10min)',
-      message
-    }); */
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!'
